f5cc4ef58038d199f42a598cbf1fb05470b5de49
arusch <arusch@geninfo.com>
2016-10-30 14:01:17 -0400

    first commit

---
+++ AdamV1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AdamV1.Startup))]
+namespace AdamV1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
